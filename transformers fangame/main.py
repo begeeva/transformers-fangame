9a9d7e5261523934c2e36bb2223c19bc9d16df37
begeeva <terfyafoka5@gmail.com>
2023-12-30 11:30:43 +0300

    изменила главное меню

--- transformers fangame/main.py
+++ transformers fangame/main.py
@@ -1,3 +1,4 @@
+from random import randrange, randint
 import pygame
 import sys
 import os
@@ -36,6 +37,27 @@ def terminate():
     sys.exit()
 
 
+class Star(pygame.sprite.Sprite):
+    image = load_image('sprites', 'a star.png')
+
+    def __init__(self, group):
+        super().__init__(*group)
+        self.image = Star.image
+        self.rect = self.image.get_rect()
+        self.rect.x = randrange(1, 768)
+        self.rect.y = randrange(1, 384)
+        self.vx = randrange(1, 5)
+        self.vy = randrange(-5, 5)
+
+    def update(self):
+        self.rect = self.rect.move(self.vx, 0)
+        self.destroy()
+
+    def destroy(self):
+        if self.rect.x > 768:
+            self.kill()
+
+
 class StartBtn(pygame.sprite.Sprite):
     not_pressed = load_image('sprites', 'start btn.png')
     pressed = load_image('sprites', 'start btn pressed.png')
@@ -58,24 +80,34 @@ class StartBtn(pygame.sprite.Sprite):
 
 def main_menu():
     all_sprites = pygame.sprite.Group()
+    star_sprites = pygame.sprite.Group()
+    all_sprites.add(star_sprites)
 
     st_btn_sprite = pygame.sprite.GroupSingle()
     start_btn = StartBtn(st_btn_sprite)
-    all_sprites.add(st_btn_sprite)
-
-    game_title = pygame.sprite.Sprite()
-    game_title.image = load_image('sprites', 'title.png')
-    game_title.rect = game_title.image.get_rect()
-    game_title.rect.x = 40
-    game_title.rect.y = 100
-    all_sprites.add(game_title)
-
-    bg = load_image('backgrounds', 'main menu bg.png')
-    screen.blit(bg, (0, 0))
     st_btn_pressed_time = 0
+
+    game_title = load_image('sprites', 'title.png')
+    game_title_rect = game_title.get_rect()
+    game_title_rect.x = 40
+    game_title_rect.y = 100
+
+    bg = load_image('backgrounds', 'space.png')
 
     pygame.mixer.music.load('sounds/The Transformers (Theme) (128kbps).mp3')
     pygame.mixer.music.play(-1)
+
+    n_stars = 500
+
+    for i in range(n_stars):
+        star = Star(star_sprites)
+        all_sprites.add(star)
+
+    cybertron = load_image('sprites', 'Cybertron.png')
+    cybertron_rect = cybertron.get_rect()
+    cybertron_rect.bottom = HEIGHT
+    cybertron_rect.right = WIDTH
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -84,7 +116,22 @@ def main_menu():
                 if start_btn.rect.collidepoint(event.pos):
                     start_btn.update(event)
                     st_btn_pressed_time = pygame.time.get_ticks()
+
+        screen.blit(bg, (0, 0))
+
+        if len(all_sprites.sprites()) < 502:
+            star = Star(star_sprites)
+            star.rect.x = 0
+            all_sprites.add(star)
+
         all_sprites.draw(screen)
+        all_sprites.update()
+
+        screen.blit(cybertron, cybertron_rect)
+        screen.blit(game_title, game_title_rect)
+        screen.blit(start_btn.image, start_btn.rect)
+        start_btn.update()
+
         if start_btn.is_pressed and 300 < pygame.time.get_ticks() - st_btn_pressed_time < 400:
             pygame.mixer.music.stop()
             return
